32d8845257468a9686de13964ab4fd7cc68b900a
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-09-17 20:17:41 -0400

    test out example code for loading files from /public

--- pages/api/pathfinder.js
+++ pages/api/pathfinder.js
@@ -1,13 +1,32 @@
 import fs from 'fs'
 import path from 'path'
 
+export default (req, res) => {
+  console.log("BLAH")
+
+  const dirRelativeToPublicFolder = 'img'
+  console.log(dirRelativeToPublicFolder)
+  
+  const dir = path.resolve('./public', dirRelativeToPublicFolder);
+  console.log(dir)
+
+  const filenames = fs.readdirSync(dir);
+  console.log(filenames)
+  
+  const images = filenames.map(name => path.join('/', dirRelativeToPublicFolder, name))
+  console.log(images);
+
+  res.statusCode = 200
+  res.json(images);
+}
+
 /**
  * Finds list of image paths given filter parameters agent, map, attackDefense
  * 
  * @param request
  * @param response
  * @returns {array} list of paths
- */ 
+
 export default (req, res) => {
   console.log("START")
   const { agent, map, attackDefense } = req.query;
@@ -28,3 +47,4 @@ export default (req, res) => {
   res.statusCode = 200;
   res.json(images);
 }
+ */
