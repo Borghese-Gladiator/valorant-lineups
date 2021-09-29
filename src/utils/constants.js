423eb23ff04dfbe59c36e98f99b6813df540b1b7
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-09-29 02:32:46 -0400

    Update constants & move /icons/ to their own folder (as opposed to /agents/icons)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -19,20 +19,20 @@ export const maps = [
 ]
 
 export const agents = [
-  { value: "astra", imgPath: "/img/agents/icons/Astra_icon.png", disabled: false },
-  { value: "breach", imgPath: "/img/agents/icons/Breach_icon.png", disabled: true },
-  { value: "brimstone", imgPath: "/img/agents/icons/Brimstone_icon.png", disabled: false },
-  { value: "cypher", imgPath: "/img/agents/icons/Cypher_icon.png", disabled: true },
-  { value: "jett", imgPath: "/img/agents/icons/Jett_icon.png", disabled: true },
-  { value: "kayo", imgPath: "/img/agents/icons/KAYO_icon.png", disabled: true },
-  { value: "killjoy", imgPath: "/img/agents/icons/Killjoy_icon.png", disabled: true },
-  { value: "omen", imgPath: "/img/agents/icons/Omen_icon.png", disabled: true },
-  { value: "phoenix", imgPath: "/img/agents/icons/Phoenix_icon.png", disabled: true },
-  { value: "raze", imgPath: "/img/agents/icons/Raze_icon.png", disabled: true },
-  { value: "reyna", imgPath: "/img/agents/icons/Reyna_icon.png", disabled: true },
-  { value: "sage", imgPath: "/img/agents/icons/Sage_icon.png", disabled: true },
-  { value: "skye", imgPath: "/img/agents/icons/Skye_icon.png", disabled: true },
-  { value: "sova", imgPath: "/img/agents/icons/Sova_icon.png", disabled: true },
-  { value: "viper", imgPath: "/img/agents/icons/Viper_icon.png", disabled: false },
-  { value: "yoru", imgPath: "/img/agents/icons/Yoru_icon.png", disabled: true }
+  { value: "astra", imgPath: "/img/icons/Astra_icon.png", disabled: false },
+  { value: "breach", imgPath: "/img/icons/Breach_icon.png", disabled: true },
+  { value: "brimstone", imgPath: "/img/icons/Brimstone_icon.png", disabled: false },
+  { value: "cypher", imgPath: "/img/icons/Cypher_icon.png", disabled: true },
+  { value: "jett", imgPath: "/img/icons/Jett_icon.png", disabled: true },
+  { value: "kayo", imgPath: "/img/icons/KAYO_icon.png", disabled: true },
+  { value: "killjoy", imgPath: "/img/icons/Killjoy_icon.png", disabled: true },
+  { value: "omen", imgPath: "/img/icons/Omen_icon.png", disabled: true },
+  { value: "phoenix", imgPath: "/img/icons/Phoenix_icon.png", disabled: true },
+  { value: "raze", imgPath: "/img/icons/Raze_icon.png", disabled: true },
+  { value: "reyna", imgPath: "/img/icons/Reyna_icon.png", disabled: true },
+  { value: "sage", imgPath: "/img/icons/Sage_icon.png", disabled: true },
+  { value: "skye", imgPath: "/img/icons/Skye_icon.png", disabled: true },
+  { value: "sova", imgPath: "/img/icons/Sova_icon.png", disabled: true },
+  { value: "viper", imgPath: "/img/icons/Viper_icon.png", disabled: false },
+  { value: "yoru", imgPath: "/img/icons/Yoru_icon.png", disabled: true }
 ]
