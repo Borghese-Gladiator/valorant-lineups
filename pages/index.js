c00d7aeecafe6a27286915a17e3441798da6a594
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-09-29 20:52:06 -0400

    separate by section

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import { useEffect, useRef, useState } from "react";
-import { VStack, Box, Center, Image, Wrap, WrapItem, Heading, Spacer, useDisclosure } from '@chakra-ui/react';
+import { useDisclosure, VStack, Box, Center, Image, Wrap, WrapItem, Heading, StackDivider } from '@chakra-ui/react';
 // Custom Components
 import RootLayout from "../src/components/RootLayout";
 // Utilities
 import GameDataContext from "../src/context/GameDataContext";
-import { removeFileEnding } from '../src/utils/utils';
+import { splitImgListBySitePrefix, removeFileEnding } from '../src/utils/utils';
 
 export default function HomePage({ pathsObject }) {
   // Sidebar data
@@ -38,19 +38,40 @@ export default function HomePage({ pathsObject }) {
           backgroundImage: "radial-gradient(#444cf7 1px, #e5e5f7 1px)",
           backgroundSize: "20px 20px"
         }}>
-          <Wrap justify="center" align="center" spacing="30px">
-            {imgList.map((imgPath, idx) => {
-              const filename = imgPath.replace(/^.*[\\\/]/, '')
-              return (
-                <WrapItem key={`lineup-img-${idx}`} style={{ maxWidth: "600px" }}>
-                  <VStack>
-                    <Image src={imgPath} alt="" />
-                    <Heading as="h4" size="md" syle={{ paddingTop: 0, marginTop: 0 }}>{removeFileEnding(filename)}</Heading>
-                  </VStack>
-                </WrapItem>
-              )
-            })}
-          </Wrap>
+          <VStack
+            divider={<StackDivider borderColor="gray.200" />}
+            spacing={4}
+            align="stretch"
+          >
+            {console.log(imgList)}
+            {console.log(splitImgListBySitePrefix(imgList))}
+            {
+              splitImgListBySitePrefix(imgList).map((val, idx) => {
+                const { siteName, siteImgList } = val
+                console.log(siteName, siteImgList)
+                return (
+                  <Box>
+                    <Center><Heading as="h2" size="xl">{siteName.toUpperCase()}</Heading></Center>
+                    <Wrap justify="center" align="center" spacing="30px">
+                      {
+                        siteImgList.map((imgPath, idx) => {
+                          const filename = imgPath.replace(/^.*[\\\/]/, '')
+                          return (
+                            <WrapItem key={`lineup-img-${idx}`} style={{ maxWidth: "600px" }}>
+                              <VStack>
+                                <Image src={imgPath} alt="" />
+                                <Heading as="h4" size="md" syle={{ paddingTop: 0, marginTop: 0 }}>{removeFileEnding(filename)}</Heading>
+                              </VStack>
+                            </WrapItem>
+                          )
+                        })
+                      }
+                    </Wrap>
+                  </Box>
+                )
+              })
+            }
+          </VStack>
         </Box>
       </RootLayout>
     </GameDataContext.Provider>
@@ -65,7 +86,7 @@ export async function getStaticProps() {
       .filter(dirent => dirent.isDirectory())
       .map(dirent => dirent.name)
   }
-  // recursively builds pathsObject
+  // recursively builds pathsObject ( eg: { "viper": { "ascent": { "attack": ["/viper/ascent/attack/a-site.png"] }}} )
   const recBuildObject = (obj, fullPath) => {
     const dirList = getDirectories(fullPath).map((directory, idx) => {
       obj[directory] = recBuildObject({}, path.join(fullPath, directory))
@@ -73,13 +94,14 @@ export async function getStaticProps() {
     if (dirList.length === 0) {
       // BASE CASE - no subdirectories left
       const imgList = fs.readdirSync(fullPath).map((filename, idx) => {
-        const imgStaticPath = fullPath.split('public')[1] // Next.js loads from /img/llineups...
+        const imgStaticPath = fullPath.split('public')[1] // Next.js loads from /img/lineups...path etc.
         return path.join(imgStaticPath, filename)
       })
       return imgList
     }
     return obj
   }
+
   const lineupsDir = path.join(process.cwd(), 'public', 'img', 'lineups')
   const pathsObject = recBuildObject({}, lineupsDir);
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,3 +5,23 @@ export function capitalizeFirstLetter(string) {
 export function removeFileEnding(str) {
   return str.replace(/\.[^/.]+$/, "")
 }
+
+
+// Builds array of Site objects from array of img path strings
+export function splitImgListBySitePrefix(imgPathList) {
+  const arr = []
+  for (const imgPath of imgPathList) {
+    const siteName = imgPath.replace(/^.*[\\\/]/, '').split('-').slice(0, 1)[0]
+    const idx = arr.findIndex((obj) => obj.siteName === siteName)
+    if (idx === -1) {
+      arr.push({
+        siteName: siteName,
+        siteImgList: [imgPath]
+      })
+    } else {
+      arr[idx].siteImgList.push(imgPath)
+    }
+  }
+  console.log(arr)
+  return arr
+}
